869955337a55a9e685ef99bf88c119f52042af38
null <mywei1989@gmail.com>
2015-10-01 14:28:52 +0800

    登录功能开发

--- models/user.js
+++ models/user.js
@@ -1,12 +1,39 @@
+var settings = require('../settings/settings.js');
 var MongoClient = require('./db.js');
 
+
 function User(user){
-  this.name = user.name;
-  this.password = user.password;
+  this.userName = user.userName;
+  this.userPassword = user.userPassword;
+}
+
+
+
+User.prototype.login = function(callback){
+  var user = {
+    userName:this.userName,
+    userPassword:this.userPassword
+  };
+
+  MongoClient.connect(settings.mongoUrl,function(err,db){
+      var collection = db.collection('users');
+      /*collection.insert({
+        userName:user.userName,
+        userPassword:user.userPassword
+      },function(err,result){
+        console.log(result);
+        db.close();
+      });*/
+      collection.findOne({userName:user.userName,userPassword:user.userPassword},function(err,result){
+        if((!err)&&result){
+          callback&&callback(true);
+        }else{
+          callback&&callback(false);
+        }
+        db.close();
+      });
+
+    });
 }
 
 module.exports = User;
-
-User.prototype.login = function(user,callback){
-
-}
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var settings = require('../settings/settings.js');
-var MongoClient = require('../models/db');
+/*var MongoClient = require('../models/db');*/
+var User = require('../models/user.js');
 
 module.exports = function(app){
   app.get('/login',function(req,res){
@@ -11,19 +12,19 @@ module.exports = function(app){
   });
 
   app.post('/login',function(req,res){
-    var user = req.body;
-    console.log(user);
-    MongoClient.connect(settings.mongoUrl,function(err,db){
-      var collection = db.collection('users');
-      collection.insert({
-        userName:user.userName,
-        userPassword:user.userPassword
-      },function(err,result){
-        console.log(result);
-        db.close();
-      });
-
+    var loginForm = req.body;
+    var userLogin = new User({
+      userName: loginForm.userName,
+      userPassword: loginForm.userPassword
     });
+    userLogin.login(function(result){
+      if(result){
+        console.log('登录成功');
+      }else{
+        console.log('登录失败');
+      }
+    });
+
     res.redirect('/login');
 
   });
